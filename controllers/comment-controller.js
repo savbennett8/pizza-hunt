e67fb45bef3d898f6c16c4d85e878c6a5a407857
Savannah Bennett <savvy.bennett8@gmail.com>
2021-05-20 12:13:33 -0600

    comment create & delete

---
+++ controllers/comment-controller.js
@@ -0,0 +1,50 @@
+const { Comment, Pizza } = require('../models');
+
+const commentController = {
+    //add comment to pizza
+    addComment({ params, body }, res) {
+        console.log(body);
+        Comment.create(body)
+            .then(({ _id }) => {
+                return Pizza.findOneAndUpdate(
+                    { _id: params.pizzaId },
+                    //built-in MongoDB functions start w/ '$'
+                    { $push: { comments: _id } },
+                    { new: true }
+                );
+            })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(e => res.json(e));
+    },
+
+    //delete comment
+    removeComment({ params }, res) {
+        Comment.findOneAndDelete({ _id: params.commentId })
+            .then(deletedComment => {
+                if (!deletedComment) {
+                    return res.status(404).json({ message: 'No comment with this id!' })
+                }
+                return Pizza.findOneAndUpdate(
+                    { _id: params.pizzaId },
+                    { $pull: { comments: params.commentId } },
+                    { new: true }
+                );
+            })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(e => res.json(e));
+    }
+};
+
+module.exports = commentController;
