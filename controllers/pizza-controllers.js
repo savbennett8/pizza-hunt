66ebf910b2c970f8e777c6d1f34113ef38cf5968
Savannah Bennett <savvy.bennett8@gmail.com>
2021-05-22 11:18:19 -0600

    comment routes created

--- controllers/pizza-controllers.js
+++ controllers/pizza-controllers.js
@@ -4,6 +4,15 @@ const pizzaController = {
     //get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                //minus sign indicates we do not want this field returned from the comment's populated data
+                select: '-__v'
+            })
+            //exclude this field from the pizza data as well
+            .select('-__v')
+            //sort in DESC order by _id value
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(e => {
                 console.log(e);
@@ -15,6 +24,11 @@ const pizzaController = {
     //restructure 'req' to only use params from it since that's all we need
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
