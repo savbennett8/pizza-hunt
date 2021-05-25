d626b4b8416b978c723b0e7b28e37ea13abcf4eb
Savannah Bennett <savvy.bennett8@gmail.com>
2021-05-24 22:59:59 -0600

    getPizza() when clicking 'see discussion'

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -9,43 +9,65 @@ const $newCommentForm = document.querySelector('#new-comment-form');
 
 let pizzaId;
 
+function getPizza() {
+  //get id of pizza
+  const searchParams = new URLSearchParams(document.location.search.substring(1));
+  const pizzaId = searchParams.get('id');
+
+  //get pizzaInfo
+  fetch(`/api/pizzas/${pizzaId}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error({ message: 'Something went wrong!' });
+      }
+
+      return response.json();
+    })
+    .then(printPizza)
+    .catch(e => {
+      console.log(e);
+      alert('Cannot find a pizza with this id! Taking you back.');
+      window.history.back();
+    });
+}
+
 function printPizza(pizzaData) {
-    console.log(pizzaData);
+  console.log(pizzaData);
 
-    pizzaId = pizzaData._id;
+  pizzaId = pizzaData._id;
 
-    const { pizzaName, createdBy, createdAt, size, toppings, comments } = pizzaData;
+  const { pizzaName, createdBy, createdAt, size, toppings, comments } = pizzaData;
 
-    $pizzaName.textContent = pizzaName;
-    $createdBy.textContent = createdBy;
-    $createdAt.textContent = createdAt;
-    $size.textContent = size;
-    $toppingsList.innerHTML = toppings
-        .map(topping => `<span class="col-auto m-2 text-center btn">${topping}</span>`)
-        .join('');
+  $pizzaName.textContent = pizzaName;
+  $createdBy.textContent = createdBy;
+  $createdAt.textContent = createdAt;
+  $size.textContent = size;
+  $toppingsList.innerHTML = toppings
+    .map(topping => `<span class="col-auto m-2 text-center btn">${topping}</span>`)
+    .join('');
 
-    if (comments && comments.length) {
-        comments.forEach(printComment);
-    } else {
-        $commentSection.innerHTML = '<h4 class="bg-dark p-3 rounded">No comments yet!</h4>';
-    }
+  if (comments && comments.length) {
+    comments.forEach(printComment);
+  } else {
+    $commentSection.innerHTML = '<h4 class="bg-dark p-3 rounded">No comments yet!</h4>';
+  }
 }
 
 function printComment(comment) {
-    // make div to hold comment and subcomments
-    const commentDiv = document.createElement('div');
-    commentDiv.classList.add('my-2', 'card', 'p-2', 'w-100', 'text-dark', 'rounded');
+  // make div to hold comment and subcomments
+  const commentDiv = document.createElement('div');
+  commentDiv.classList.add('my-2', 'card', 'p-2', 'w-100', 'text-dark', 'rounded');
 
-    const commentContent = `
+  const commentContent = `
       <h5 class="text-dark">${comment.writtenBy} commented on ${comment.createdAt}:</h5>
       <p>${comment.commentBody}</p>
       <div class="bg-dark ml-3 p-2 rounded" >
         ${comment.replies && comment.replies.length
-            ? `<h5>${comment.replies.length} ${comment.replies.length === 1 ? 'Reply' : 'Replies'
-            }</h5>
+      ? `<h5>${comment.replies.length} ${comment.replies.length === 1 ? 'Reply' : 'Replies'
+      }</h5>
         ${comment.replies.map(printReply).join('')}`
-            : '<h5 class="p-1">No replies yet!</h5>'
-        }
+      : '<h5 class="p-1">No replies yet!</h5>'
+    }
       </div>
       <form class="reply-form mt-3" data-commentid='${comment._id}'>
         <div class="mb-3">
@@ -61,12 +83,12 @@ function printComment(comment) {
       </form>
   `;
 
-    commentDiv.innerHTML = commentContent;
-    $commentSection.prepend(commentDiv);
+  commentDiv.innerHTML = commentContent;
+  $commentSection.prepend(commentDiv);
 }
 
 function printReply(reply) {
-    return `
+  return `
   <div class="card p-2 rounded bg-secondary">
     <p>${reply.writtenBy} replied on ${reply.createdAt}:</p>
     <p>${reply.replyBody}</p>
@@ -75,40 +97,42 @@ function printReply(reply) {
 }
 
 function handleNewCommentSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const commentBody = $newCommentForm.querySelector('#comment').value;
-    const writtenBy = $newCommentForm.querySelector('#written-by').value;
+  const commentBody = $newCommentForm.querySelector('#comment').value;
+  const writtenBy = $newCommentForm.querySelector('#written-by').value;
 
-    if (!commentBody || !writtenBy) {
-        return false;
-    }
+  if (!commentBody || !writtenBy) {
+    return false;
+  }
 
-    const formData = { commentBody, writtenBy };
+  const formData = { commentBody, writtenBy };
 }
 
 function handleNewReplySubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    if (!event.target.matches('.reply-form')) {
-        return false;
-    }
+  if (!event.target.matches('.reply-form')) {
+    return false;
+  }
 
-    const commentId = event.target.getAttribute('data-commentid');
+  const commentId = event.target.getAttribute('data-commentid');
 
-    const writtenBy = event.target.querySelector('[name=reply-name]').value;
-    const replyBody = event.target.querySelector('[name=reply]').value;
+  const writtenBy = event.target.querySelector('[name=reply-name]').value;
+  const replyBody = event.target.querySelector('[name=reply]').value;
 
-    if (!replyBody || !writtenBy) {
-        return false;
-    }
+  if (!replyBody || !writtenBy) {
+    return false;
+  }
 
-    const formData = { writtenBy, replyBody };
+  const formData = { writtenBy, replyBody };
 }
 
 $backBtn.addEventListener('click', function () {
-    window.history.back();
+  window.history.back();
 });
 
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
+
+getPizza();
